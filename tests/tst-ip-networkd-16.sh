0a6c574c6299e9e509d867c1dac093affc470871
Stefan Schubert <schubi@suse.de>
2026-09-03 16:27:22 +0200

    mixing ipv6 addresses with ipv4 should not be done

--- tests/tst-ip-networkd-16.sh
+++ tests/tst-ip-networkd-16.sh
@@ -13,12 +13,11 @@ cleanup()
     exit $exit_code
 }
 
-trap cleanup EXIT
 
 
 TEMPDIR=$(mktemp -d)
 
-./rdii-networkd -o "$TEMPDIR" -a ip=[2001:1234:56:8f63::10]:[2001:1234:56:8f63::2]:[2001:1234:56:8f63::1]:64:hogehoge:eth0:on
+./rdii-networkd -o "$TEMPDIR" -a ip=[2001:1234:56:8f63::10]:[2001:1234:56:8f63::2]:[2001:1234:56:8f63::1]:64:hogehoge:eth0:either6
 
 for cfg in "${TEMPDIR}"/*; do
     cfg=$(basename "$cfg")
